0495e2e5880117d066c1db7557e617a7b95bca5a
pkalaga1 <prava.kalaga@gmail.com>
2018-02-09 17:48:08 -0500

    Created skeleton for File Reader

--- TrainClassifier.py
+++ TrainClassifier.py
@@ -1,3 +1,12 @@
-from sklearn import MLPClassifier
+from sklearn import MLPClassifier, externals
 
 class TrainClassifier:
+
+
+
+
+def main():
+    cls = TrainClassifier()
+
+if __name__ == "__main__":
+    main()
