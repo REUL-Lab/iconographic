44fe1fa2865bf6f3e33733faf1d1bc1a87056aa9
DerianHaas <bsqrdminus4ac@gmail.com>
2018-02-21 17:52:07 -0500

    Switched firebase to python api

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from FileReader import *
 from flask import *
+import pyrebase
+import sys
 app = Flask(__name__)
-
+config = {
+    "apiKey": "AIzaSyBNeqANlaGZOcRX0aT_i1YfDbiCpTgfHqI",
+    "authDomain": "flickering-fire-3792.firebaseapp.com",
+    "databaseURL": "https://flickering-fire-3792.firebaseio.com",
+    "projectId": "flickering-fire-3792",
+    "storageBucket": "flickering-fire-3792.appspot.com",
+    "messagingSenderId": "189921954509"
+}
+firebase = pyrebase.initialize_app(config)
+user = None
 
 @app.route('/', methods=['GET','POST'])
 def start():
@@ -9,11 +20,36 @@ def start():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    global user
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            user = firebase.auth().sign_in_with_email_and_password(username, password)
+            return redirect('/admin')
+        except Exception as e:
+            return render_template('login.html', error=e)
+    else:
+        if user is None:
+            return render_template('login.html', error="")
+        else:
+            return redirect('/admin')
 
-@app.route('/admin', )
+@app.route('/logout')
+def logout():
+    global user
+    user = None
+    return redirect('/login')
+
+@app.route('/admin')
 def admin():
-    return render_template('admin.html')
+    global user
+    if user is None:
+        return redirect('/login')
+    else:
+        db = firebase.database()
+        labels = db.child("labels").get().val()
+        return render_template('admin.html', labels=labels)
 
 @app.route('/main')
 def main():
