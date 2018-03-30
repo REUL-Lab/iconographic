9e1ea7d23ba6b23f86910596381e7926bad126b2
achiang31 <alan.chiang@live.com>
2018-03-29 20:56:25 -0400

    flask output

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def analyzefile():
 
             out = open("output.txt", "w+")
             for k in iconlist.keys():
-                out.write("\r\n" + iconlist[k] + "\r\n" + "\r\n" + k + "\r\n" + "\r\n" + "----------" + "\r\n" + "\r\n")
+                out.write("\n" + iconlist[k] + "\n" + "\n" + k + "\n" + "\n" + "----------" + "\n" + "\n")
             out.close()
 
 
