a8057a09bd016761d64c62a8c26755a7085865d6
DerianHaas <bsqrdminus4ac@gmail.com>
2018-03-30 15:55:42 -0400

    merged with alan

--- FileReader.py
+++ FileReader.py
@@ -6,41 +6,6 @@ import pickle
 class FileReader:
 
 
-    # labelnames = [
-    #     "None",
-    #     "Selling data",
-    #     "Gag wrap",
-    #     "Data Retention after termination",
-    #     "Data Retention after logout",
-    #     "Data tracking after deletion",
-    #     "Data tracking after logout",
-    #     "Data tracking after closing",
-    #     "Location tracking",
-    #     "Location tracking retention",
-    #     "Third Party Data Share (eg: sharing data w/ police)",
-    #     "Sell UGC",
-    #     "Own UGC",
-    #     "Repurpose UGC",
-    #     "Use of UGC after termination",
-    #     "Recurring charges",
-    #     "Termination conditions",
-    #     "Arbitration Clause",
-    #     "Forced Termination (Code of Conduct)",
-    #     "Removal of UGC",
-    #     "Notice of EULA changes",
-    #     "Under 18 liability",
-    #     "Lacks 13+ clause for use",
-    #     "Expiration Conditions",
-    #     "User’s Ability to Transfer Product",
-    #     "Indemnity Clause",
-    #     "Embedded Policies (when you have to click out to something else to read, but agree to in EULA)",
-    #     "Wild Card",
-    #     "Email subscriptions (spam etc.)",
-    #     "EULA Version date",
-    #     "Maintenance (eg: software updates, etc.)"
-    # ]
-
-
     @staticmethod
     def textSplit(text):
 
@@ -66,9 +31,6 @@ class FileReader:
         data = vectorizer.transform(rawdata)
         labels = classifier.predict(data)
 
-
-        #labelvals = [FileReader.labelnames[num] for num in labels]
-
         return dict(zip(rawdata, labels))
 
     @staticmethod
@@ -85,7 +47,6 @@ class FileReader:
         vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
         data = vectorizer.transform(rawdata)
         labels = classifier.predict(data)
-       # labelvals = [FileReader.labelnames[num] for num in labels]
 
         return dict(zip(rawdata, labels))
 
